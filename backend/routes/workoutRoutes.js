4a071c8a59c444eebabb3c22b875500d3433816e
Piyumal Madhuwantha <121705632+piyumal2105@users.noreply.github.com>
2024-12-25 23:40:21 +0530

    change the add workout route api endpoint

--- backend/routes/workoutRoutes.js
+++ backend/routes/workoutRoutes.js
@@ -4,7 +4,7 @@ import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", authMiddleware, addWorkout);
+router.post("/add", authMiddleware, addWorkout);
 router.get("/", authMiddleware, getWorkouts);
 
 export default router;
